d944c4931f4102aebf0952cef5ac96b648692386
Gerorge Liao <george.liao@canonical.com>
2025-07-26 14:31:11 +0200

    [client] added client part code.

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,22 @@
+use hello::HelloRequest;
+use hello::greeter_client::GreeterClient;
+use tonic::Request;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+pub mod hello {
+    tonic::include_proto!("hello");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = Request::new(HelloRequest {
+        name: "Rustacean".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("Response = {:?}", response.into_inner().message);
 
     Ok(())
 }
