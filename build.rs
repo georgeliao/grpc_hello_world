8470d7faa827deff7c8270681c23ac3fb2a6f8b4
Gerorge Liao <george.liao@canonical.com>
2025-07-26 14:30:42 +0200

    [build] formatted the build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
-fn main()-> Result<(), Box<dyn std::error::Error>>
-{
-    tonic_build::compile_protos("proto/hello_world.proto")?;
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::compile_protos("proto/hello.proto")?;
     Ok(())
 }
