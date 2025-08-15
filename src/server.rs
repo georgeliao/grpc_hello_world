328d8a59208dd3b1538df3829c8803f9fe0560a8
Gerorge Liao <george.liao@canonical.com>
2025-08-15 22:30:03 +0200

    [server] clean up mtls part code.

--- src/server.rs
+++ src/server.rs
@@ -33,18 +33,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server_key = fs::read("../../tls/server_key.pem").await?;
     let identity = Identity::from_pem(server_cert, server_key);
 
-    // --- OPTIONAL (mTLS): trust this CA for client certs ---
-    // If you want to require client certs signed by multipass_root_cert.pem:
-    // let client_ca = Certificate::from_pem(fs::read("../../tls/multipass_root_cert.pem").await?);
-
     let addr = "[::1]:50051".parse()?;
     println!("GreeterServer (TLS) listening on {}", addr);
 
     Server::builder()
-        .tls_config(
-            ServerTlsConfig::new().identity(identity), // Comment this out to disable mTLS (server-only TLS).
-                                                       // .client_ca_root(client_ca),
-        )?
+        .tls_config(ServerTlsConfig::new().identity(identity))?
         .add_service(GreeterServer::new(MyGreeter::default()))
         .serve(addr)
         .await?;
